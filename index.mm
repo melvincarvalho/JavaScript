6014a5bfd7a66f98c6191353e26493ce2cdc5bf5
Melvin Carvalho <melvincarvalho@gmail.com>
2019-10-07 21:22:32 +0200

    DEFAULT_MESSAGE

--- index.mm
+++ index.mm
@@ -63,68 +63,29 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
-<node TEXT="make your own!" POSITION="right" ID="ID_1389208206" CREATED="1570293607657" MODIFIED="1570294749709">
-<icon BUILTIN="wizard"/>
-<edge COLOR="#ff0000"/>
-<node TEXT="fork this repo" ID="ID_1738668143" CREATED="1570293615063" MODIFIED="1570385431199" LINK="https://github.com/solidpayorg/webmaps/">
-<icon BUILTIN="full-1"/>
-</node>
-<node TEXT="on the command line" ID="ID_1188995541" CREATED="1570294688300" MODIFIED="1570385435773">
-<icon BUILTIN="full-2"/>
-<node TEXT="clone your fork" ID="ID_653799488" CREATED="1570294695677" MODIFIED="1570294700683"/>
-</node>
-<node TEXT="open the index.mm file with freeplane" ID="ID_1013629200" CREATED="1570293620249" MODIFIED="1570385439727">
-<icon BUILTIN="full-3"/>
-<node TEXT="get freeplane!" ID="ID_1320208052" CREATED="1570294588057" MODIFIED="1570294616385" LINK="https://www.freeplane.org/wiki/index.php/Home"/>
-</node>
-<node TEXT="start editing!" ID="ID_1410176299" CREATED="1570294727092" MODIFIED="1570385444177">
-<icon BUILTIN="full-4"/>
-</node>
-<node TEXT="export to html" ID="ID_278465196" CREATED="1570293629843" MODIFIED="1570385449064">
-<icon BUILTIN="full-5"/>
-<node TEXT="1. file menu" ID="ID_347519982" CREATED="1570294621891" MODIFIED="1570385383055">
-<icon BUILTIN="full-1"/>
-</node>
-<node TEXT="2. click export map" ID="ID_1364431254" CREATED="1570294628510" MODIFIED="1570385392532">
-<icon BUILTIN="full-2"/>
-</node>
-<node TEXT="3. choose XHTML (clickable map)" ID="ID_65834416" CREATED="1570294661504" MODIFIED="1570385397891">
-<icon BUILTIN="full-3"/>
-</node>
-</node>
-<node TEXT="one the command line run" ID="ID_668125997" CREATED="1570293641632" MODIFIED="1570385453495">
-<icon BUILTIN="full-6"/>
-<node TEXT="bin/process.html" ID="ID_417353516" CREATED="1570293651983" MODIFIED="1570293659194"/>
-<node TEXT="bin/deploy" ID="ID_1316516546" CREATED="1570293660875" MODIFIED="1570293664489"/>
-</node>
-<node TEXT="you are done!" ID="ID_1463249056" CREATED="1570293683863" MODIFIED="1570385457988">
-<icon BUILTIN="full-7"/>
-<node TEXT="test your webmap!" ID="ID_1342426050" CREATED="1570293666284" MODIFIED="1570293681404">
-<node TEXT="it will be at &lt;username&gt;.github.io/webmaps" ID="ID_791049591" CREATED="1570294771137" MODIFIED="1570294799091"/>
-</node>
-</node>
-</node>
-<node TEXT="star this repo" POSITION="left" ID="ID_634574866" CREATED="1570294548787" MODIFIED="1570294809542" LINK="https://github.com/solidpayorg/webmaps/">
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="made using webmaps" POSITION="left" ID="ID_634574866" CREATED="1570294548787" MODIFIED="1570476048397" LINK="https://github.com/solidpayorg/webmaps/">
 <icon BUILTIN="bookmark"/>
 <edge COLOR="#0000ff"/>
 </node>
-<node TEXT="examples" POSITION="left" ID="ID_428021122" CREATED="1570295118138" MODIFIED="1570448748245">
-<icon BUILTIN="button_ok"/>
-<edge COLOR="#00ff00"/>
-<node ID="ID_1055482545" CREATED="1570295130322" MODIFIED="1570295130322" LINK="https://lightning.money/"><richcontent TYPE="NODE">
+<node TEXT="Video" POSITION="right" ID="ID_849215328" CREATED="1570476065863" MODIFIED="1570476114594">
+<edge COLOR="#ff00ff"/>
+<node ID="ID_1384859362" CREATED="1570476124707" MODIFIED="1570476124707" LINK="https://bxjs.dev/"><richcontent TYPE="NODE">
 
 <html>
   <head>
     
   </head>
   <body>
-    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://lightning.money/">https://lightning.money/</a> 
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://bxjs.dev/">https://bxjs.dev/</a> 
   </body>
 </html>
+
 </richcontent>
 </node>
-<node TEXT="more" ID="ID_1185474668" CREATED="1570475388941" MODIFIED="1570475395867" LINK="examples/index.mm"/>
+</node>
+<node TEXT="language" POSITION="right" ID="ID_1566689685" CREATED="1570476128893" MODIFIED="1570476132171">
+<edge COLOR="#00ffff"/>
 </node>
 </node>
 </map>
