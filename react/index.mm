46d4b5bd95f83254e2b60e80893bca510266bd83
Melvin Carvalho <melvincarvalho@gmail.com>
2019-10-23 10:07:05 +0200

    Sections for React

--- react/index.mm
+++ react/index.mm
@@ -63,11 +63,7 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
-<node TEXT="meta frameworks" POSITION="right" ID="ID_1544605098" CREATED="1571227413610" MODIFIED="1571227417749">
-<edge COLOR="#00ffff"/>
-<node TEXT="iconic" ID="ID_662028730" CREATED="1571227427047" MODIFIED="1571227434154" LINK="https://ionicframework.com/blog/announcing-ionic-react/"/>
-</node>
+<hook NAME="AutomaticEdgeColor" COUNTER="16" RULE="ON_BRANCH_CREATION"/>
 <node TEXT="links" POSITION="left" ID="ID_990086409" CREATED="1571160460734" MODIFIED="1571160488688">
 <icon BUILTIN="attach"/>
 <edge COLOR="#ff00ff"/>
@@ -75,6 +71,39 @@
 <node TEXT="roadmap" ID="ID_1313044671" CREATED="1571810538956" MODIFIED="1571810954742" LINK="https://github.com/adam-golab/react-developer-roadmap">
 <node TEXT="mine" ID="ID_63308067" CREATED="1571810963274" MODIFIED="1571810970442" LINK="roadmap.mm"/>
 </node>
+<node TEXT="meta frameworks" ID="ID_1544605098" CREATED="1571227413610" MODIFIED="1571817991015">
+<node TEXT="iconic" ID="ID_662028730" CREATED="1571227427047" MODIFIED="1571227434154" LINK="https://ionicframework.com/blog/announcing-ionic-react/"/>
+</node>
+</node>
+<node TEXT="Build Tools" POSITION="right" ID="ID_724688107" CREATED="1571811593039" MODIFIED="1571817987453">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Styling" POSITION="right" ID="ID_1267363076" CREATED="1571811599841" MODIFIED="1571817987459">
+<edge COLOR="#007c00"/>
+</node>
+<node TEXT="State Management" POSITION="right" ID="ID_1952310218" CREATED="1571811607571" MODIFIED="1571817987462">
+<edge COLOR="#7c007c"/>
+</node>
+<node TEXT="Type Checkers" POSITION="right" ID="ID_1289375581" CREATED="1571811805261" MODIFIED="1571817987470">
+<edge COLOR="#007c7c"/>
+</node>
+<node TEXT="Form Helpers" POSITION="right" ID="ID_1385748350" CREATED="1571811821518" MODIFIED="1571817987471">
+<edge COLOR="#7c7c00"/>
+</node>
+<node TEXT="Routing" POSITION="right" ID="ID_1228318416" CREATED="1571811825328" MODIFIED="1571817987472">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="API Clients" POSITION="right" ID="ID_1434625848" CREATED="1571811832714" MODIFIED="1571817987482">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Utility Libraries" POSITION="right" ID="ID_1862350426" CREATED="1571811842635" MODIFIED="1571817987483">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Testing" POSITION="right" ID="ID_1596624615" CREATED="1571811851473" MODIFIED="1571817987484">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="i18n" POSITION="right" ID="ID_454994441" CREATED="1571811854648" MODIFIED="1571817987487">
+<edge COLOR="#00ffff"/>
 </node>
 </node>
 </map>
