9303ae9e7076d3dbe849f0f96748afd74db241f2
Melvin Carvalho <melvincarvalho@gmail.com>
2019-10-25 07:33:20 +0200

    Add array links

--- arrays.mm
+++ arrays.mm
@@ -103,18 +103,18 @@
 <icon BUILTIN="go"/>
 <edge COLOR="#00007c"/>
 <font SIZE="12" BOLD="true"/>
-<node TEXT="concat" ID="ID_442337071" CREATED="1571980331067" MODIFIED="1571980332597"/>
-<node TEXT="indexOf" ID="ID_350597574" CREATED="1571980336041" MODIFIED="1571980338961"/>
-<node TEXT="join" ID="ID_1430084916" CREATED="1571980348129" MODIFIED="1571980349568"/>
+<node TEXT="concat" ID="ID_442337071" CREATED="1571980331067" MODIFIED="1571981523113" LINK="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat"/>
+<node TEXT="indexOf" ID="ID_350597574" CREATED="1571980336041" MODIFIED="1571981509571" LINK="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf"/>
+<node TEXT="join" ID="ID_1430084916" CREATED="1571980348129" MODIFIED="1571981480769" LINK="https://melvincarvalho.github.io/JavaScript/arrays.html"/>
 <node TEXT="includes" ID="ID_1792313021" CREATED="1571980333402" MODIFIED="1571980670158" LINK="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes">
 <node TEXT="var array1 = [1, 2, 3];&#xa;&#xa;console.log(array1.includes(2));&#xa;// expected output: true" ID="ID_1406062870" CREATED="1571980691899" MODIFIED="1571980692754"/>
 </node>
-<node TEXT="lastIndexOf" ID="ID_659535653" CREATED="1571980350401" MODIFIED="1571980353775"/>
-<node TEXT="slice" ID="ID_1574549482" CREATED="1571980354516" MODIFIED="1571980356093"/>
+<node TEXT="lastIndexOf" ID="ID_659535653" CREATED="1571980350401" MODIFIED="1571981535358" LINK="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf"/>
+<node TEXT="slice" ID="ID_1574549482" CREATED="1571980354516" MODIFIED="1571981549720" LINK="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice"/>
 <node TEXT="to" ID="ID_1004688462" CREATED="1571980361420" MODIFIED="1571980363939">
-<node TEXT="Source" ID="ID_1988183786" CREATED="1571980364982" MODIFIED="1571980377998"/>
-<node TEXT="String" ID="ID_799815160" CREATED="1571980367441" MODIFIED="1571980369904"/>
-<node TEXT="LocaleString" ID="ID_1313686839" CREATED="1571980371193" MODIFIED="1571980375538"/>
+<node TEXT="Source" ID="ID_1988183786" CREATED="1571980364982" MODIFIED="1571981562077" LINK="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/toSource"/>
+<node TEXT="String" ID="ID_799815160" CREATED="1571980367441" MODIFIED="1571981572014" LINK="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/toString"/>
+<node TEXT="LocaleString" ID="ID_1313686839" CREATED="1571980371193" MODIFIED="1571981583047" LINK="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/toLocaleString"/>
 </node>
 </node>
 <node TEXT="mutator" POSITION="right" ID="ID_721627699" CREATED="1571978829050" MODIFIED="1571980819607" COLOR="#990000" HGAP_QUANTITY="-104.49999646842491 pt" VSHIFT_QUANTITY="-32.99999901652339 pt">
