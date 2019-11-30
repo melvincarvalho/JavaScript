b031f99d7d623bc9cd7dfff407c4065fafcd94da
Melvin Carvalho <melvincarvalho@gmail.com>
2019-11-30 21:15:45 +0100

    add information map software

--- apps/index.mm
+++ apps/index.mm
@@ -1,7 +1,7 @@
 <map version="freeplane 1.7.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="apps" FOLDED="false" ID="ID_131836552" CREATED="1574897110329" MODIFIED="1574897128819" LINK="../index.mm">
-<edge STYLE="bezier" COLOR="#00ffff" WIDTH="thin" DASH="SOLID"/>
+<node TEXT="apps" FOLDED="false" ID="ID_131836552" CREATED="1574897110329" MODIFIED="1575144927748" LINK="../index.mm" STYLE="oval">
+<edge STYLE="bezier" DASH="SOLID"/>
 <hook NAME="MapStyle">
     <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
 
@@ -64,8 +64,10 @@
 </map_styles>
 </hook>
 <hook NAME="AutomaticEdgeColor" COUNTER="17" RULE="ON_BRANCH_CREATION"/>
-<node TEXT="mindmap" POSITION="right" ID="ID_1505022052" CREATED="1574897135608" MODIFIED="1574897137715">
+<font SIZE="18"/>
+<node TEXT="information maps" POSITION="right" ID="ID_1505022052" CREATED="1574897135608" MODIFIED="1575144909175">
 <edge COLOR="#7c0000"/>
+<font BOLD="true"/>
 <node ID="ID_1008171028" CREATED="1574884067176" MODIFIED="1574884067176" LINK="https://github.com/alyssaxuu/flowy"><richcontent TYPE="NODE">
 
 <html>
@@ -76,6 +78,32 @@
     <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://github.com/alyssaxuu/flowy">https://github.com/alyssaxuu/flowy</a> 
   </body>
 </html>
+</richcontent>
+</node>
+<node ID="ID_1060403936" CREATED="1575144536809" MODIFIED="1575144536809" LINK="https://github.com/jagenjo/litegraph.js#projects-using-it"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://github.com/jagenjo/litegraph.js#projects-using-it">https://github.com/jagenjo/litegraph.js#projects-using-it</a> 
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_260563962" CREATED="1575144649171" MODIFIED="1575144649171" LINK="https://github.com/ssshooter/mind-elixir-core"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <a http-equiv="content-type" content="text/html; charset=utf-8" href="https://github.com/ssshooter/mind-elixir-core">https://github.com/ssshooter/mind-elixir-core</a> 
+  </body>
+</html>
+
 </richcontent>
 </node>
 </node>
