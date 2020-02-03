d71916cbbaa3164fb2ae2a1b0752c3aea4e8bcfd
Melvin Carvalho <melvincarvalho@gmail.com>
2020-02-03 20:47:46 +0100

    add redux

--- state/redux.mm
+++ state/redux.mm
@@ -1,7 +1,6 @@
 <map version="freeplane 1.7.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="state" FOLDED="false" ID="ID_1287476678" CREATED="1575899050104" MODIFIED="1578925504725" LINK="../index.mm" STYLE="oval">
-<font SIZE="18"/>
+<node TEXT="Redux" FOLDED="false" ID="ID_512653034" CREATED="1575899050104" MODIFIED="1580759071881" LINK="index.mm" STYLE="oval">
 <edge STYLE="bezier" COLOR="#7c0000" WIDTH="thin" DASH="SOLID"/>
 <hook NAME="MapStyle">
     <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
@@ -65,8 +64,40 @@
 </map_styles>
 </hook>
 <hook NAME="AutomaticEdgeColor" COUNTER="22" RULE="ON_BRANCH_CREATION"/>
-<node TEXT="Redux" POSITION="right" ID="ID_497084437" CREATED="1580759046380" MODIFIED="1580759046383" LINK="redux.mm">
-<edge STYLE="bezier" COLOR="#7c0000" WIDTH="thin"/>
+<font SIZE="18"/>
+<node TEXT="A predictable state container for JavaScript apps" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1351253301" CREATED="1575899068275" MODIFIED="1580759143020" STYLE="bubble">
+<font BOLD="true"/>
+</node>
+<node TEXT="Terms" POSITION="right" ID="ID_1711994767" CREATED="1575899224275" MODIFIED="1580759186520">
+<icon BUILTIN="pencil"/>
+<font BOLD="true"/>
+<node TEXT="Store" ID="ID_1671723188" CREATED="1575899239069" MODIFIED="1575899246977">
+<font BOLD="true"/>
+<node TEXT="The whole state of your app is stored in an object tree inside a single store." ID="ID_1425829042" CREATED="1575899232098" MODIFIED="1575899232098"/>
+</node>
+<node TEXT="Action" ID="ID_1102222808" CREATED="1575899256772" MODIFIED="1575899262558">
+<font BOLD="true"/>
+<node TEXT="The only way to change the state tree is to emit an action, an object describing what happened." ID="ID_988909752" CREATED="1575899232098" MODIFIED="1575899232098"/>
+</node>
+<node TEXT="Reducer" ID="ID_1202078005" CREATED="1575899269516" MODIFIED="1575899276114">
+<font BOLD="true"/>
+<node TEXT="To specify how the actions transform the state tree, you write pure reducers." ID="ID_1702006703" CREATED="1575899232106" MODIFIED="1575899232106"/>
+</node>
+</node>
+<node TEXT="links" POSITION="left" ID="ID_997703536" CREATED="1580759073557" MODIFIED="1580759075681">
+<edge COLOR="#7c7c00"/>
+<node TEXT="Redux" ID="ID_487950240" CREATED="1580759091724" MODIFIED="1580759164869" LINK="https://redux.js.org/">
+<font BOLD="true"/>
+</node>
+<node TEXT="Getting Started" ID="ID_1778052371" CREATED="1575899120537" MODIFIED="1580759109413" LINK="https://redux.js.org/introduction/getting-started">
+<font BOLD="true"/>
+</node>
+<node TEXT="examples" ID="ID_1613255975" CREATED="1575899420351" MODIFIED="1580759204761" LINK="https://redux.js.org/introduction/examples">
+<font BOLD="true"/>
+</node>
+<node TEXT="codesandbox" ID="ID_1254200183" CREATED="1575633622987" MODIFIED="1580759201703" LINK="https://codesandbox.io/s/reduxjs-joqhu">
+<font BOLD="true"/>
+</node>
 </node>
 </node>
 </map>
